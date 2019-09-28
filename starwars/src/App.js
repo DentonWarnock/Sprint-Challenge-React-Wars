7f8c06daa82a17822c6a9b00c3f7e42ca12d7332
DentonWarnock <dentonwarnock@gmail.com>
2019-09-27 18:47:22 -0700

    Data request from API working and is being saved in state data.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from "axios";
+import Card from "./components/Card.js";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +10,26 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [ person, setPerson ] = useState();
+
+  useEffect(() => {
+    async function fetchData() {
+      try{
+        const data = await axios.get("https://swapi.co/api/people/")
+        setPerson(data)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  console.log(person);
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <Card />
     </div>
   );
 }
