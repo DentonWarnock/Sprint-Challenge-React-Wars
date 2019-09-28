fc339e496590f0d5fc608d34235677991ba29b4b
DentonWarnock <dentonwarnock@gmail.com>
2019-09-27 19:00:36 -0700

    All names are displaying on page.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -10,13 +10,13 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-  const [ person, setPerson ] = useState();
+  const [ person, setPerson ] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
       try{
-        const data = await axios.get("https://swapi.co/api/people/")
-        setPerson(data)
+        const data = await axios.get("https://swapi.co/api/people/");
+        setPerson(data.data.results);
       } catch (error) {
         console.log(error);
       }
@@ -29,7 +29,9 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-      <Card />
+      {person.map((value, index) => {
+        return <Card value={value} key={index} />
+      })}
     </div>
   );
 }
--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -3,6 +3,6 @@ import React from "react";
 export default function Card(props) {
 
   return (
-    <h1>Test Card</h1>
+    <h1>{props.value.name}</h1>
   )
 }
